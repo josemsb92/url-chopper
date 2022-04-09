ceaf57795fbde765e8f245134a212fb7b88638cd
josemsb92 <josemsb92@gmail.com>
2022-04-09 17:35:39 +0200

    postUrl now doesn´t take any url that doesn't starts by https

--- controllers/postUrl.js
+++ controllers/postUrl.js
@@ -1,9 +1,15 @@
 const urlManager = require("../managers/urlManager");
 //short-unique-id library generates an short uuid.
 const ShortUniqueId = require('short-unique-id');
+const { CURSOR_FLAGS } = require("mongodb");
 const uid = new ShortUniqueId({ length: 6 });
 async function postUrl(req, res) {
-  const url = await urlManager.createShortUrl({...req.body,GeneratedUrl:`${uid()}`}) 
+  const {OriginalUrl} = req.body
+  const url =
+  OriginalUrl.startsWith("https") ? 
+     await urlManager.createShortUrl({OriginalUrl,GeneratedUrl:`${uid()}`}) : "undefined"
+  
+  
   res.status(201).json({ url: url }).end();
 }
 
