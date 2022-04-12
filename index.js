092462c36d2e50e8b26739c5a2f7010f2c8ba4e4
josemsb92 <josemsb92@gmail.com>
2022-04-12 23:14:56 +0100

    cors added

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 const PORT = process.env.PORT || 3002
-
+const cors = require('cors');
+//Provisional Cors all origin alloweds, needs to be changed.
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/urlchopper', require('./routes'))
