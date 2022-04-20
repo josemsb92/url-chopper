db6fcda9dd9571ff2348bf0713cc2aa92809d8e5
josemsb92 <josemsb92@gmail.com>
2022-04-20 22:47:04 +0200

    controller modified to validate if isURL

--- controllers/postUrl.js
+++ controllers/postUrl.js
@@ -1,16 +1,20 @@
 const urlManager = require("../managers/urlManager");
 //short-unique-id library generates an short uuid.
-const ShortUniqueId = require('short-unique-id');
-const { CURSOR_FLAGS } = require("mongodb");
+const ShortUniqueId = require("short-unique-id");
 const uid = new ShortUniqueId({ length: 6 });
+var validator = require("validator");
+
 async function postUrl(req, res) {
-  const {OriginalUrl,UserId} = req.body
-  
-  const url =
-  OriginalUrl.startsWith("https")? 
-     await urlManager.createShortUrl({OriginalUrl,GeneratedUrl:`${uid()}`, UserId}) : "undefined"
-  
-  
+  const { OriginalUrl, UserId } = req.body;
+
+  const url = validator.isURL(OriginalUrl)
+    ? await urlManager.createShortUrl({
+        OriginalUrl,
+        GeneratedUrl: `${uid()}`,
+        UserId,
+      })
+    : "undefined";
+
   res.status(201).json({ url: url }).end();
 }
 
