9ddc32de18cc0b75d318e58edd735fadd30e13f5
josemsb92 <josemsb92@gmail.com>
2022-04-21 00:05:17 +0200

    url Body has been corrected to GeneratedUrl

--- routes/url.js
+++ routes/url.js
@@ -2,5 +2,5 @@ require('../mongoose')
 
 const router = require('express').Router();
 router.post(('/'), require('../controllers/postUrl'))
-router.get(('/:shorturl'), require('../controllers/getUrlData'))
+router.get(('/:GeneratedUrl'), require('../controllers/getUrlData'))
 module.exports = router
